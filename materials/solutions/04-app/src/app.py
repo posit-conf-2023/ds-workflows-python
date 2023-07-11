94ba7672b06f7fce56024db8a51da9e8c2708d97
SamEdwardes <sam.edwardes@posit.co>
2023-07-11 15:41:55 -0700

    App is looking good

--- materials/solutions/04-app/src/app.py
+++ materials/solutions/04-app/src/app.py
@@ -1,29 +1,17 @@
-from pathlib import Path
-from textwrap import dedent
-import pandas as pd
+import datetime
+
 import numpy as np
+import pandas as pd
 import shinyswatch
+from ipyleaflet import (Map, Marker, MarkerCluster, ScaleControl)
 from ipywidgets import HTML
-from ipyleaflet import Map, Marker, MarkerCluster, ScaleControl, Popup, CircleMarker
-from pins import board_connect
-from shiny import App, ui, render, reactive
-from shinywidgets import output_widget, reactive_read, register_widget, render_widget
-from rich.console import Console
+from shiny import App, reactive, render, ui
+import ibis
+from shinywidgets import (output_widget, render_widget)
 
-from .data import load_map_data
 from .console import console
+from .data import create_db_connection, get_zip_code_options, get_license_code_options
 
-# Global data
-RAW_MAP_DATA = load_map_data()
-
-ZIP_CODE_OPTIONS = {}
-for zip_code in RAW_MAP_DATA["zip_code"].to_list():
-    ZIP_CODE_OPTIONS[zip_code] = zip_code
-
-LICENSE_CODE_OPTIONS = {} 
-for location_license_data in RAW_MAP_DATA["license_data"].tolist():
-    for i in location_license_data:
-        LICENSE_CODE_OPTIONS[i["license_code"]] = i["license_code"] + " - " + i["license_description"]
 
 
 # Part 1: ui ----
@@ -34,22 +22,31 @@ app_ui = ui.page_navbar(
         ui.layout_sidebar(
             ui.panel_sidebar(
                 ui.tags.h5("Filters"),
-                ui.input_select(id="filter_zip_code", label="Zip Code", choices=ZIP_CODE_OPTIONS),
-                ui.input_select(id="filter_license_code", label="License Type", choices=LICENSE_CODE_OPTIONS),
+                ui.tags.hr(),
+                ui.input_select(id="filter_zip_code", label="Zip Code", choices={'': 'All'}),
+                ui.input_select(id="filter_license_code", label="License Type", choices={'': 'All'}),
+                ui.input_select(id="filter_license_active", label="License Active", choices=["Yes", "No", "Show everything"]),
                 ui.input_text(id="filter_name", label="Business Name", placeholder="Search for a business by name", value=None),
+                ui.tags.br(),
+                ui.tags.h5("Pagination"),
+                ui.tags.hr(),
+                ui.input_numeric(id="filter_number_of_rows", label="Number of Rows", value=100, step=50),
+                ui.row(
+                    ui.column(6, ui.output_ui("last_page_button_placeholder")),
+                    ui.column(6, ui.output_ui("next_page_button_placeholder")),
+                ),
             ),
             ui.panel_main(
-                ui.tags.h5("Map"),
-                output_widget("leaflet_map"),
-                ui.tags.h5("Businesses"),
-                ui.output_data_frame("business_table_output"),
+                ui.navset_tab(
+                    ui.nav("Table", ui.output_data_frame("business_table_output")),
+                    ui.nav("Map", output_widget("leaflet_map")),
+                )
             )
         )
     ),
     ui.nav(
         "About",
         ui.markdown("""
-
 ##### About this app
 
 This app is used to help City of Chicago Food Inspectors identify which
@@ -71,36 +68,149 @@ If you identify any issues please submit a bug report [here](https://github.com/
 )
 
 # Part 2: server ----
-def server(input, output, session): 
+def server(input, output, session):
+    
+    # Create a database connection
+    con = create_db_connection()
+    
+    # Track pagination
+    page_number = reactive.Value(1)
+    
+    
+    @reactive.Effect
+    @reactive.event(input.next_page_button)
+    def increment_pagination():
+        page_number.set(page_number() + 1)
+        console.log(f"Incrementing pagination (page={page_number()})...")
 
+
+    @reactive.Effect
+    @reactive.event(input.last_page_button)
+    def decrement_pagination():
+        page_number.set(page_number() - 1)
+        console.log(f"Decrementing pagination (page={page_number()})....")
+
+
+    @reactive.Effect
+    @reactive.event(input.filter_zip_code, input.filter_license_code, input.filter_name)
+    def reset_pagination():
+        """
+        Whenever a new filter is applied reset the pagination
+        """
+        page_number.set(1)
+        console.log(f"Resetting pagination (page={page_number()})....")
+    
+    
+    @output
+    @render.ui
+    def last_page_button_placeholder():
+        if page_number() == 1:
+            css_class = "disabled"
+        else:
+            css_class = ""
+            
+        return ui.input_action_button(
+            "last_page_button", 
+            f"Last Page ({page_number() - 1})", 
+            width="100%",
+            class_=css_class
+        )
+
+
+    @output
+    @render.ui
+    def next_page_button_placeholder():
+        return ui.input_action_button(
+            "next_page_button", 
+            f"Next Page ({page_number() + 1})", 
+            width="100%"
+        )
+    
+    @reactive.Effect()
+    def update_zip_code_options():
+        ui.update_select(
+            "filter_zip_code",
+            choices=get_zip_code_options(con=con),
+            selected=""
+        )
+
+    @reactive.Effect()
+    def update_license_type_options():
+        ui.update_select(
+            "filter_license_code",
+            choices=get_license_code_options(con=con),
+            selected=""
+        )
+
+    
     @reactive.Calc
-    def filter_data():
+    def filter_data():        
         console.log("Filtering data...")
+        
         # Get filter values
         selected_name = input.filter_name()
         selected_zip_code = input.filter_zip_code()
         selected_license_code = input.filter_license_code()
+        selected_license_active = input.filter_license_active()
         
-        # Get raw data
-        df = RAW_MAP_DATA.copy()
-        console.log(f"Original data has {df.shape[0]:,} rows.")
+        # Create base query
+        license_table = con.table("business_license_validated")
+        
+        # Only show business that are subject to food inspection
+        inspections_table = con.table("food_inspection_validated")[["license_"]]
+        license_table = license_table.inner_join(
+            inspections_table, 
+            license_table.license_id == inspections_table.license_
+        )
         
         # Apply filters
         if selected_zip_code:
             console.log(f"Filtering on Zip Code: {selected_zip_code}")
-            df = df.loc[df["zip_code"] == selected_zip_code]
-        
+            license_table = license_table[license_table.zip_code == selected_zip_code]
+            
         if selected_license_code:
             console.log(f"Filtering on License Code: {selected_license_code}")
-            df = df.loc[df["license_data"].apply(lambda x: any([i["license_code"] == selected_license_code for i in x])), :]
-        
+            license_table = license_table[license_table.license_code == selected_license_code]
+            
+        if selected_license_active == "Yes":
+            console.log("Filtering on active licenses")
+            license_table = license_table[license_table.expiration_date >= datetime.datetime.now()]
+        elif selected_license_active == "No":
+            console.log("Filtering on inactive licenses")
+            license_table = license_table[license_table.expiration_date < datetime.datetime.now()]
+ 
         if selected_name:
             console.log(f"Filtering on Doing Business as Name: {selected_name}")
-            df = df.loc[df["doing_business_as_name"].str.contains(selected_name, case=False) | df["legal_name"].str.contains(selected_name, case=False)]
-
-        console.log(f"Filtered data has {df.shape[0]:,} rows.")
-
-        return df
+            license_table = license_table[license_table.doing_business_as_name.contains(selected_name.upper())]
+            
+        # Handle pagination
+        page = page_number()
+        limit = input.filter_number_of_rows()
+        offset = (page - 1) * limit
+        console.log(f"Pagination: page={page}, offset={offset}, limit={limit}")
+        license_table = license_table.limit(limit, offset=offset)
+        # console.log(ibis.show_sql(license_table))
+        
+        df = license_table.to_pandas()
+        
+        # Get predictions.
+        df = predict_likelihood_of_violation(df)
+        
+        # Reorder columns
+        first_cols = [
+            "risk",
+            "doing_business_as_name",
+            "address",
+            "license_id",
+            "license_code",
+            "license_description"
+        ]
+        
+        col_order = first_cols + [i for i in df.columns if i not in first_cols]
+        
+        console.log("Filtering complete.")
+        
+        return df.loc[:, col_order]
 
 
     @output
@@ -108,8 +218,90 @@ def server(input, output, session):
     def leaflet_map():
         """
         Create and register map.
-        """
-        map = create_map(df=filter_data())
+        """   
+        console.log("Rendering map...")
+        map = Map(
+            center=(41.881832,  -87.623177), 
+            zoom=11, 
+            scroll_wheel_zoom=True,
+        )
+
+        # Add a distance scale
+        map.add_control(ScaleControl(position="bottomleft"))
+        
+        # Clean up the data so that there is only one location per row. One location
+        # could have multiple licenses
+        df = filter_data()
+        
+        # Only keep a subset of the columns that are relevant for mapping.
+        map_cols = [
+            "legal_name", 
+            "doing_business_as_name",
+            "address", 
+            "zip_code",
+            "latitude",
+            "longitude",
+            "license_id",
+            "license_code",
+            "license_description",
+            "license_start_date",
+            "expiration_date",
+        ]
+        
+        # Apply the data cleaning steps.
+        map_data = (
+            df
+            # .head(100_000)
+            .loc[:, map_cols]
+            .drop_duplicates()
+            .reset_index(drop=True)
+            .groupby([
+                "legal_name", 
+                "doing_business_as_name",
+                "address", 
+                "zip_code",
+                "latitude",
+                "longitude"
+            ])
+            .apply(lambda x: [{
+                "license_id": row["license_id"], 
+                "license_code": row["license_code"], 
+                "license_description": row["license_description"],
+                "license_start_date": row["license_start_date"],
+                "expiration_date": row["expiration_date"],
+            } for _, row in x.iterrows()])
+            .reset_index()
+            .rename({0: "license_data"}, axis=1)
+        )
+
+        # Add markers
+        markers = []
+        for _, row in map_data.iterrows():
+            lat = row["latitude"]
+            lon =  row["longitude"]
+            name =  row["doing_business_as_name"]
+            license_data = row["license_data"]
+            
+            marker = Marker(
+                location=(lat, lon),
+                title=name,
+                draggable=False,
+            )
+            
+            msg = HTML(f"""
+                <p><b>Name:</b> {name}</p>
+                <b>Licenses:</b>
+                <ul>
+                    {"".join([f"<li> {i['license_id']} - {i['license_code']} - {i['license_description']} </li>" for i in license_data])}
+                </ul>
+                """
+            )
+            marker.popup = msg
+            markers.append(marker)
+        
+        marker_cluster = MarkerCluster(markers=markers)
+        map.add_layer(marker_cluster)
+        console.log("Rendering map complete")
         return map
 
 
@@ -121,59 +313,14 @@ def server(input, output, session):
         filters.
         """
         return filter_data()
-    
-    # @output
-    # @render.w
-    
-
-def create_map(df: pd.DataFrame) -> Map:
-    """
-    Function to create the initial ipyleaflet Map object.
-    """
-    # Initialize and display when the session starts
-    console.log("Rendering map...")
-    console.log("Rendering map...")
-    map = Map(
-        center=(41.881832,  -87.623177), 
-        zoom=11, 
-        scroll_wheel_zoom=True
-    )
-
-    # Add a distance scale
-    map.add_control(ScaleControl(position="bottomleft"))
-
-    # Add markers
-    markers = []
-    for index, row in df.iterrows():
-        lat = row["latitude"]
-        lon =  row["longitude"]
-        name =  row["doing_business_as_name"]
-        license_data = row["license_data"]
-        marker = Marker(
-            location=(lat, lon),
-            title=name,
-            draggable=False,
-        )
-        marker.popup = HTML(value="".join([
-            f"<p><b>Name:</b> {name}</p>",
-            f"<p><b>Licenses:</b></p>",
-            f"""<ul>{" ".join([f'<li>{i["license_code"]} - {i["license_description"]}</li>' for i in license_data])}</ul>"""
-        ]))
-        markers.append(marker)
-    marker_cluster = MarkerCluster(markers=markers)
-    map.add_layer(marker_cluster)
-    console.log("Rendering map complete")
-
-    return map
 
 
-
-
-def predict_likelihood_of_violation(df: pd.DataFrame) -> pd.Series:
+def predict_likelihood_of_violation(df: pd.DataFrame) -> pd.DataFrame:
     # TODO: call the vetiver API once Gagan has finished creating it.
     # Right now this is just a placeholder function...
-    preds = np.random.rand(df.shape[0])
-    return pd.Series(preds)
+    out = df.copy()
+    out["risk"] = np.random.rand(out.shape[0])
+    return out
 
 
 # Combine into a shiny app.
--- materials/solutions/04-app/src/data.py
+++ materials/solutions/04-app/src/data.py
@@ -1,23 +1,63 @@
 from pathlib import Path
 import pandas as pd
 from pins import board_connect
+import os
+import ibis
 
 from .console import console
 
-def load_map_data() -> pd.DataFrame:
+
+def create_db_connection() -> ibis.BaseBackend:
+    db_user = "posit"
+    db_password = os.environ["CONF23_DB_PASSWORD"]
+    db_host = os.environ["CONF23_DB_HOST"]
+    db_port = 5432
+    db_database = "python_workshop"
+
+    con = ibis.postgres.connect(
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port,
+        database=db_database
+    )
+    return con
+
+
+def get_zip_code_options(con: ibis.BaseBackend) -> dict[str, str]:
     """
-    Function to load the business license data.
+    A helper function to generate all of the options for the zip code dropdown.
     """
-    # For development only. Temporarily store the data in the ~/Downloads directory
-    # to make reloading the app faster.
-    cache_path = Path("~/Downloads/business-data.arrow").expanduser()
-    if cache_path.is_file():
-        console.log("Reading data from cache...")
-        df = pd.read_feather(cache_path)
-    else:
-        console.log("Reading data from Connect...")
-        board = board_connect(server_url='https://colorado.posit.co/rsc/')
-        df = board.pin_read("sam.edwardes/chicago-business-map-data")
-        # Cache to disk...
-        df.to_feather(cache_path)
-    return df
+    console.log("Getting zip code options...")
+
+    data = con.table("business_license_validated") \
+        [["zip_code"]] \
+        .order_by([ibis.desc("zip_code")]) \
+        .distinct() \
+        .to_pandas()["zip_code"] \
+        .tolist()
+    options = {'': 'All'} | {i: i for i in data}
+    return options
+
+
+def get_license_code_options(con: ibis.BaseBackend) -> dict[str, str]:
+    """
+    A helper function to generate all of the options for the license code 
+    dropdown.
+    """
+    console.log("Getting license code options...")   
+
+    df = con.table("business_license_validated") \
+        [["license_code", "license_description"]] \
+        .distinct() \
+        .to_pandas()
+    
+    options = {'': 'All'}
+    
+    for _, row in df.iterrows():
+        key = row["license_code"]
+        value = row["license_code"] + " - " + row["license_description"]
+        options[key] = value
+
+    return options
+        
